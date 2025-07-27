dd256a60bc49e815ca4b17858aaa6d753f88bfbc
Harshitha K <harshitha02012005@gmail.com>
2025-07-27 19:54:30 +0530

    Update QN_05_Harshitha.java

--- QN_05_Harshitha.java
+++ QN_05_Harshitha.java
@@ -3,13 +3,12 @@ class count{
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
-    int sol;
-    if(n==0)
-      sol=0;
-    else if(n%9==0)
-      sol=9;
-    else
-      sol=n%9;
+    int sol=0;
+    while(n>0){
+      int dig=n%10;
+      sol+=1;
+      n/=10;
+    }
     System.out.print(sol);
   }
 }
