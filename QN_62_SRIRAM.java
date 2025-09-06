fb57d9124587e12fb3c73ebd34016f8fefee0332
SRI RAM S <144365989+sriram2915@users.noreply.github.com>
2025-09-06 20:05:19 +0530

    Implement selection sort algorithm in Java

---
+++ QN_62_SRIRAM.java
@@ -0,0 +1,22 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+        for (int num : arr) System.out.print(num + " ");
+    }
+}
